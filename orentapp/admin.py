fd9b7f4e4d912a4af69ee076a3374900ccb9cd38
thomas <tom.huglo@gmail.com>
2015-05-13 19:07:10 +0200

    django admin customization & orentapp admin.

---
+++ orentapp/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from orentapp.models import Product, Use, Profil
+
+
+# Profil Administration => Inline with User Administration
+class ProfilAdmin(admin.StackedInline):
+    model = Profil
+    can_delete = False
+    verbose_name_plural = 'Profils'
+
+
+# User Administration
+class UserAdmin(UserAdmin):
+    inlines = (ProfilAdmin, )
+
+
+# Product Administration
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'cost', 'post_date', 'update_date', 'is_public', 'private_group')
+
+
+# Use Administration
+class UseAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'date')
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Use, UseAdmin)
